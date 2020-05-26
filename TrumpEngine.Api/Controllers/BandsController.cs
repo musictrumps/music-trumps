4d928a9483aeae4d21ec2478010811c8d4d1c31d
Gabriel Cardoso <gabreguarnieri@gmail.com>
2020-05-26 18:17:13 +0100

    add api using .net core 3.1

---
+++ TrumpEngine.Api/Controllers/BandsController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrumpEngine.Core;
+
+namespace TrumpEngine.Api.Controllers
+{
+    [ApiController]
+    public class BandsController : ControllerBase
+    {
+        [HttpGet("{genre}")]
+        [Route("/api/bands/{genre}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult  Get(string genre)
+        {
+            BandCore bandCore = new BandCore();
+            var bands = bandCore.GetBandsByGenre(genre);
+            return Ok(bands);
+        }
+    }
+}
