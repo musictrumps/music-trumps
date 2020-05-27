7247274ebb32626e26dda2046826e2d6b496e5d5
Thiago Sanches <tsigwt@gmail.com>
2020-05-27 16:03:37 -0300

    Add a simple insertion mechanism to the firebase

--- TrumpEngine.Console/Program.cs
+++ TrumpEngine.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using TrumpEngine.Core;
+﻿using System.Net.Http;
+using System.Text;
+using TrumpEngine.Core;
+using TrumpEngine.Model;
 
 namespace TrumpEngine.Console
 {
@@ -6,14 +9,25 @@ namespace TrumpEngine.Console
     {
         static void Main(string[] args)
         {
+            string genre = "rock";
             BandCore bandCore = new BandCore();
-            var bands = bandCore.GetBandsByGenre("rock");
+            var bands = bandCore.GetBandsByGenre(genre);
 
             foreach (var band in bands)
             {
                 System.Console.WriteLine("Band: {0}, Year: {1}, Picture: {2}", 
                     band.Name, band.Begin.Year, band.Picture);
+
+                InsertBandToFirebase(genre, band);
             }
+        }
+
+        static async void InsertBandToFirebase(string genre, Band band)
+        {
+            HttpClient client = new HttpClient();
+            var stringContent = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(band), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(string.Format("https://music-trumps-database.firebaseio.com/{0}.json", genre), stringContent);
+            System.Console.WriteLine(await response.Content.ReadAsStringAsync());
         }
     }
 }
