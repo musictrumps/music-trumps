a88ed4e65bfcac0cb36318c7a0d43da7841a704b
Gabriel <gabriel@gmst.ie>
2020-06-03 20:52:20 +0100

    add app secrets to the tests project

--- TrumpEngine.UnitTests/BasicMethodsTest.cs
+++ TrumpEngine.UnitTests/BasicMethodsTest.cs
@@ -1,16 +1,33 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Microsoft.Extensions.Configuration;
 using TrumpEngine.Core;
+using TrumpEngine.Data;
+using TrumpEngine.Shared;
+using TrumpEngine.Shared.Settings;
 
 namespace TrumpEngine.UnitTests
 {
     [TestClass]
     public class BasicMethodsTest
     {
+        IConfiguration Configuration { get; set; }
+        private readonly Settings _settings;
+        public BasicMethodsTest()
+        {
+            // the type specified here is just so the secrets library can 
+            // find the UserSecretId we added in the csproj file
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<BasicMethodsTest>();
+
+            Configuration = builder.Build();
+            _settings = Configuration.Get<Settings>();
+        }
+
         [TestMethod]
         public void TestReturnsAListOfBandsByGenreWithFilledProperties()
         {
-            var core = new BandCore();
+            var core = new BandCore(new BandData(_settings));
             var bands = core.GetBandsByGenre("rock");
 
             bool isNameBlank = bands.Exists(b => string.IsNullOrWhiteSpace(b.Name));
