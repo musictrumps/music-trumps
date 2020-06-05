c84e9f45816992127bf0e2839488b37415cfa738
Gabriel <gabriel@gmst.ie>
2020-06-05 17:32:28 +0100

    start using AddEnvironmentVariablesto the api startup configuration

--- TrumpEngine.Api/Program.cs
+++ TrumpEngine.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace TrumpEngine.Api
@@ -12,6 +13,10 @@ namespace TrumpEngine.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
