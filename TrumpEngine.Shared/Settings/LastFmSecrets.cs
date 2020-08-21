3f64a69ecbb9c8845bc9a838501fb00159cae2dd
Thiago <tsigwt@gmail.com>
2020-08-21 17:35:10 -0300

    Update LastFmSecrets.cs

--- TrumpEngine.Shared/Settings/LastFmSecrets.cs
+++ TrumpEngine.Shared/Settings/LastFmSecrets.cs
@@ -2,23 +2,7 @@ using System.IO;
 
 namespace TrumpEngine.Shared
 {
-    public class SpotifySecrets
+    public class LastFmSecrets
     {
-        public string BaseUrl { get; set; }
-        public string TokenUrl { get; set; }
-        public string PathRecommendationsByGenre { get; set; }
-        public string PathArtistInformation { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-
-        public string GetUrlByGenre()
-        {
-            return Path.Combine(BaseUrl, PathRecommendationsByGenre);
-        }
-
-        public string GetUrlArtistInformation()
-        {
-            return Path.Combine(BaseUrl, PathArtistInformation);
-        }
     }
 }
