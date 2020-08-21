99eb85c0d1c2182d9315f0df97452a0921fa3052
Thiago <tsigwt@gmail.com>
2020-08-21 17:35:43 -0300

    Update LastFm.cs

--- TrumpEngine.Data/Providers/Implementation/LastFm.cs
+++ TrumpEngine.Data/Providers/Implementation/LastFm.cs
@@ -7,14 +7,13 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using TrumpEngine.Data.Providers.Implementation.Model;
-using TrumpEngine.Data.Providers.Interface;
 using TrumpEngine.Model;
 using TrumpEngine.Shared;
 using TrumpEngine.Shared.Settings;
 
 namespace TrumpEngine.Data.Providers.Implementation
 {
-    internal class LastFm : IProvider
+    internal class LastFm
     {
     }
 }
