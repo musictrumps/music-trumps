03aefd3ee5e78bb524e5fbfbe4856529926ab893
Thiago <tsigwt@gmail.com>
2020-08-22 10:41:49 -0300

    Update LastFmArtistInfo.cs

--- TrumpEngine.Data/Providers/Implementation/Model/LastFmArtistInfo.cs
+++ TrumpEngine.Data/Providers/Implementation/Model/LastFmArtistInfo.cs
@@ -1,1 +1,12 @@
 
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace TrumpEngine.Data.Providers.Implementation.Model
+{
+    internal class LastFmArtistInfo
+    {
+        [JsonProperty(PropertyName = "tracks")]
+        public List<Track> Tracks { get; set; }
+    }
+}
