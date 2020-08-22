cbf548d7393f15c0c9884932b2466dd0958c3522
Thiago <tsigwt@gmail.com>
2020-08-22 10:40:07 -0300

    Update LastFm.cs

--- TrumpEngine.Data/Providers/Implementation/LastFm.cs
+++ TrumpEngine.Data/Providers/Implementation/LastFm.cs
@@ -16,9 +16,24 @@ namespace TrumpEngine.Data.Providers.Implementation
     internal class LastFm
     {
         private readonly LastFmSecrets _lastFmSecrets;
+        private const string LASTFM_API_URL = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist={0}&api_key={1}&format=json";
+        
         public LastFm(LastFmSecrets lastFmSecrets)
         {
             _lastFmSecrets = lastFmSecrets;
         }
+        
+        public string GetInfo(string artist) 
+        {
+            string summary = null;
+                using (System.Net.WebClient web = new System.Net.WebClient())
+                {
+                    string response = web.DownloadString(string.Format(LASTFM_API_URL, _lastFmSecrets.ApiKey));
+                    json = JsonConvert.DeserializeObject<RecommendedTracks>(response);
+                }
+
+                return summary;    
+        }
+        
     }
 }
